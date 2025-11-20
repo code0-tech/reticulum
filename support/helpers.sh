6a634da45d99676b1d8d4950a5dc6b8db1c475ab
Niklas van Schrick <mc.taucher2003@gmail.com>
2025-11-20 19:42:45 +0100

    Add fail flag to curl

--- support/helpers.sh
+++ support/helpers.sh
@@ -3,7 +3,7 @@ function download_project() {
   version=$(cat versions/$project)
   echo "Downloading $project at $version"
   mkdir -p projects
-  curl --output projects/$project.tar.gz -L "https://github.com/code0-tech/$project/archive/$version.tar.gz" || return 10
+  curl --output projects/$project.tar.gz --fail-with-body -L "https://github.com/code0-tech/$project/archive/$version.tar.gz" || return 10
   tar -xzf projects/$project.tar.gz -C projects || return 10
   rm projects/$project.tar.gz || return 1
   mv projects/$project-* projects/$project || return 1
